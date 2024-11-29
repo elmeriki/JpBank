59975b032e13f961b5c507bcc6741d2e0232ba15
baden djiwason <badendjiwason@gmail.com>
2024-11-29 12:46:55 +0200

    I corrected my first name baden on line eight

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("Hello Fritzgates");
 console.log("Chialuka Foy");
 console.log("hello fredtambe");
 console.log("hello obendang");
-console.log("Hello badenp powel");
+console.log("Hello baden powel");
 
 
 
